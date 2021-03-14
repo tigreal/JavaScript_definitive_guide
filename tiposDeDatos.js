3363d59092e2b1b2af9ccac88e4c513cbf472776
tigreal <fosmaralvarez@gmail.com>
2021-03-13 23:22:44 -0400

    tipos de variables inconpleto

---
+++ tiposDeDatos.js
@@ -0,0 +1,16 @@
+x=1; //numero
+x=0,001; // numero real
+x="hola munto";//cadena de texto
+x=true; //valor buleano
+x=false; // valor buleano
+x=null;//valor especial, significa sin valor
+x=undefined;//valor especial como nulo
+let book={//los objetos estan entre llaves
+tema:"JavaScript",//la propiedad tien un valor "JavaScript"
+edicion:7//la propiedad edicion tien el valor 7
+};
+//accede a las propiedades de un objeto con un "."
+//o con corchetes
+book.tema;
+book["edicion"];
+book.author="Flanagan";//crea nuevas propiedades por asignacion
